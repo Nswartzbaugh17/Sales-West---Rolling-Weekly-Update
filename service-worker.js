2b909d371b0bf161a8289ed4ad9cfb6f4a319c9e
Nswartzbaugh17 <nathanjswartzbaugh@gmail.com>
2025-09-13 14:47:47 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,5 @@
-const CACHE_NAME = 'salesdash-v3';
-const ASSETS = [
-  './index.html',
-  './site.webmanifest',
-  './icon-192.png',
-  './icon-512.png',
-  './apple-touch-icon.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((resp) => resp || fetch(event.request)));
-});
+const CACHE_NAME='salesdash-v5';
+const ASSETS=['./index.html','./site.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
