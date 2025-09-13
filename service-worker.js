153a1a31beaa6941e132c9e16b3731fd083c284e
Nswartzbaugh17 <nathanjswartzbaugh@gmail.com>
2025-09-13 14:43:05 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,14 @@
-
-const CACHE_NAME = 'salesdash-v1';
+const CACHE_NAME = 'salesdash-v3';
 const ASSETS = [
-  './Rolling_Sales_Dashboard_PWA.html',
+  './index.html',
   './site.webmanifest',
-  './service-worker.js',
   './icon-192.png',
   './icon-512.png',
   './apple-touch-icon.png'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (event) => {
@@ -20,7 +16,5 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then((resp) => resp || fetch(event.request)));
 });
